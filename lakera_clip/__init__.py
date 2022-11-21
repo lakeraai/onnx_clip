0823a4b1ed95ea3b6393cf28ebb8850d2cc49a72
dantimbrell <dantimbrell@gmail.com>
2022-11-21 09:25:01 +0100

    begin imports

---
+++ lakera_clip/__init__.py
@@ -0,0 +1,14 @@
+from .model import (
+    model
+)
+from .preprocessor import (
+    preprocess
+)
+from .tokenizer import (
+    Tokenizer
+)
+__all__ = [
+    "model",
+    "preprocess",
+    "Tokenizer"
+]
