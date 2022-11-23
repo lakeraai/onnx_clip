190224b0f5a6f154ad179168484ee12806df8c97
dantimbrell <dantimbrell@gmail.com>
2022-11-23 10:19:28 +0100

    reformat comments

--- lakera_clip/preprocessor.py
+++ lakera_clip/preprocessor.py
@@ -4,6 +4,10 @@ from PIL import Image
 
 
 class Preprocess:
+    """
+    A rough approximation to the CLIP `preprocess` neural net.
+    """
+    # Fixed variables that ensure the correct output shapes and values for the `Model` class.
     CLIP_INPUT_SIZE = 224
     CROP_CENTER_PADDING = 224
     NORM_MEAN = np.array([0.485, 0.456, 0.406]).reshape((1, 1, 3))
@@ -11,12 +15,15 @@ class Preprocess:
 
     def _smart_resize(self, img: Image.Image) -> np.array:
         """Resizing that preserves the image ratio"""
+        img = np.array(img)
+
         # The expected size of the image after we resize it
         # and pad to have a square format
         resized_sq_size = Preprocess.CLIP_INPUT_SIZE + 2 * Preprocess.CROP_CENTER_PADDING
+
         # Current height and width
-        img = np.array(img)
         h, w = img.shape[0:2]
+
         # The size of the image after we resize but before we pad
         if h > w:
             resized_h = resized_sq_size
@@ -25,8 +32,9 @@ class Preprocess:
             resized_w = resized_sq_size
             resized_h = round(resized_w * h / w)
 
-        # Resize preserving the ratio
+        # Resize while preserving the ratio
         img = cv.resize(img, (resized_w, resized_h), interpolation=cv.INTER_LINEAR)
+
         # Pad the image to make it square
         vert_residual = resized_sq_size - resized_h
         hor_residual = resized_sq_size - resized_w
@@ -69,6 +77,7 @@ class Preprocess:
 
         """
         self._assert_pil(img)
+
         # Resize
         img = self._smart_resize(img)
         # Crop the center
@@ -77,16 +86,19 @@ class Preprocess:
             Preprocess.CROP_CENTER_PADDING : -Preprocess.CROP_CENTER_PADDING,
         ]
 
-        # [0; 255] -> [0; 1]
+        # Normalize to values [0, 1]
         img = img / 255.0
+
         # Handle Grayscale
         if len(img.shape) == 2:
             # The NN doesn't support NxMx1 images as input
             img = np.expand_dims(img, axis=2)  # NxMx1
             img = np.concatenate((img,) * 3, axis=2)  # NxMx3
-        # Normalize
+
+        # Normalize channels
         img = (img - Preprocess.NORM_MEAN) / Preprocess.NORM_STD
-        # To tensor-friendly format
+
+        # Mimic the pytorch tensor format for Model class
         img = np.transpose(img, (2, 0, 1))
         img = img.astype(np.float32).reshape(1, 3, 224, 224)
 
