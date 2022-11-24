f418e66d7445e04420ab8babd9d399df640975d1
dantimbrell <dantimbrell@gmail.com>
2022-11-24 09:57:47 +0100

    add softmax

--- onnx_clip/__init__.py
+++ onnx_clip/__init__.py
@@ -1,5 +1,5 @@
 from .preprocessor import Preprocessor
 from .tokenizer import Tokenizer
-from .model import OnnxClip
+from .model import OnnxClip, softmax
 
-__all__ = ["Preprocessor", "Tokenizer", "OnnxClip"]
+__all__ = ["Preprocessor", "Tokenizer", "OnnxClip", "softmax"]
