02a10f4539fe84536cb5de471fe794405b1df4e6
Václav Volhejn <vv@lakera.ai>
2023-02-08 10:31:51 +0100

    Correctly handle float images, check inputs more

--- onnx_clip/preprocessor.py
+++ onnx_clip/preprocessor.py
@@ -21,17 +21,9 @@ class Preprocessor:
         (1, 1, 3)
     )
 
-    def _smart_resize(self, img: np.ndarray) -> np.ndarray:
+    @staticmethod
+    def _crop_and_resize(img: np.ndarray) -> np.ndarray:
         """Resize and crop an image to a square, preserving the aspect ratio."""
-
-        if len(img.shape) > 3:
-            raise ValueError(
-                f"The image should have 2 or 3 dimensions but got {len(img.shape)} dimensions"
-            )
-        if (len(img.shape) == 3) and img.shape[2] != 3:
-            raise ValueError(
-                f"Expected 3-channel RGB image but got image with {img.shape[2]} channels"
-            )
 
         # Current height and width
         h, w = img.shape[0:2]
@@ -63,11 +55,13 @@ class Preprocessor:
 
         if use_pil_for_resizing:
             # https://github.com/pytorch/vision/blob/7cf0f4cc1801ff1892007c7a11f7c35d8dfb7fd0/torchvision/transforms/functional_pil.py#L240
-            img_pil = Image.fromarray(img)
+            # We're working with float images but PIL uses uint8, so convert
+            # there and back again afterwards
+            img_pil = Image.fromarray((img * 255).astype(np.uint8))
             img_pil = img_pil.resize(
                 (resized_w, resized_h), resample=Image.BICUBIC
             )
-            img = np.array(img_pil)
+            img = np.array(img_pil).astype(np.float32) / 255
         else:
             img = cv.resize(
                 img, (resized_w, resized_h), interpolation=cv.INTER_CUBIC
@@ -82,15 +76,25 @@ class Preprocessor:
 
         return img
 
-    def encode_image(self, img: Union[Image.Image, np.ndarray]) -> np.ndarray:
-        """Preprocesses the images like CLIP's preprocess() function:
-        https://github.com/openai/CLIP/blob/3702849800aa56e2223035bccd1c6ef91c704ca8/clip/clip.py#L79
+    @staticmethod
+    def _image_to_float_array(img: Union[Image.Image, np.ndarray]):
+        """Converts a PIL image or a NumPy array to standard form.
+
+        Standard form means:
+        - the shape is (H, W, 3)
+        - the dtype is np.float32
+        - all values are in [0, 1]
+        - there are no NaN values
 
         Args:
-            img: PIL image or numpy array
+            img: The image to convert.
 
         Returns:
-            img: numpy image after resizing, center cropping and normalization.
+            The image converted to a NumPy array in standard form.
+
+        Raises:
+            ValueError if the image is invalid (wrong shape, invalid
+                values...).
         """
         if not isinstance(img, (Image.Image, np.ndarray)):
             raise TypeError(
@@ -101,17 +105,73 @@ class Preprocessor:
             # Convert to NumPy
             img = np.array(img)
 
-        # Resize
-        img = self._smart_resize(img)
+        if len(img.shape) > 3:
+            raise ValueError(
+                f"The image should have 2 or 3 dimensions but got "
+                f"{len(img.shape)} dimensions"
+            )
+        if (len(img.shape) == 3) and img.shape[2] != 3:
+            raise ValueError(
+                f"Expected 3-channel RGB image but got image with "
+                f"{img.shape[2]} channels"
+            )
 
-        # Normalize to values [0, 1]
-        img = img / 255.0
-
-        # Handle Grayscale
+        # Handle grayscale
         if len(img.shape) == 2:
-            # The NN doesn't support NxMx1 images as input
-            img = np.expand_dims(img, axis=2)  # NxMx1
-            img = np.concatenate((img,) * 3, axis=2)  # NxMx3
+            # The model doesn't support HxWx1 images as input
+            img = np.expand_dims(img, axis=2)  # HxWx1
+            img = np.concatenate((img,) * 3, axis=2)  # HxWx3
+
+        # At this point, `img` has the shape (H, W, 3).
+
+        if np.min(img) < 0:
+            raise ValueError(
+                "Images should have non-negative pixel values, "
+                f"but the minimum value is {np.min(img)}"
+            )
+
+        if np.issubdtype(img.dtype, np.floating):
+            if np.max(img) > 1:
+                raise ValueError(
+                    "Images with a floating dtype should have values "
+                    f"in [0, 1], but the maximum value is {np.max(img)}"
+                )
+            img = img.astype(np.float32)
+        elif np.issubdtype(img.dtype, np.integer):
+            if np.max(img) > 255:
+                raise ValueError(
+                    "Images with an integer dtype should have values "
+                    f"in [0, 255], but the maximum value is {np.max(img)}"
+                )
+            img = img.astype(np.float32) / 255
+            img = np.clip(img, 0, 1)  # In case of rounding errors
+        else:
+            raise ValueError(
+                f"The image has an unsupported dtype: {img.dtype}."
+            )
+
+        if np.isnan(img).any():
+            raise ValueError(f"The image contains NaN values.")
+
+        assert np.min(img) >= 0
+        assert np.max(img) <= 1
+        assert np.max(img) <= 1
+
+        return img
+
+    def encode_image(self, img: Union[Image.Image, np.ndarray]) -> np.ndarray:
+        """Preprocesses the images like CLIP's preprocess() function:
+        https://github.com/openai/CLIP/blob/3702849800aa56e2223035bccd1c6ef91c704ca8/clip/clip.py#L79
+
+        Args:
+            img: PIL image or numpy array
+
+        Returns:
+            img: numpy image after resizing, center cropping and normalization.
+        """
+        img = Preprocessor._image_to_float_array(img)
+
+        img = Preprocessor._crop_and_resize(img)
 
         # Normalize channels
         img = (img - Preprocessor.NORM_MEAN) / Preprocessor.NORM_STD
--- tests/test_preprocessor.py
+++ tests/test_preprocessor.py
@@ -17,10 +17,45 @@ PREPROCESSED_IMAGE_PATH = os.path.join(
 )
 
 
-def test_bad_input_type():
+def test_bad_inputs():
     pre = Preprocessor()
     with pytest.raises(TypeError):
         pre.encode_image("this should raise an error")
+
+    with pytest.raises(ValueError):
+        pre.encode_image(np.zeros((10, 10, 3), dtype=object))
+
+    with pytest.raises(ValueError):
+        pre.encode_image(np.zeros((10, 10, 1)))
+
+    with pytest.raises(ValueError):
+        img = np.zeros((10, 10, 3), dtype=np.float32)
+        img[2, 2, 0] = np.nan
+        pre.encode_image(img)
+
+    with pytest.raises(ValueError):
+        img = np.zeros((10, 10, 3), dtype=np.float32)
+        img[2, 2, 0] = 1.00001
+        pre.encode_image(img)
+
+    with pytest.raises(ValueError):
+        img = np.zeros((10, 10, 3), dtype=np.float32)
+        img[2, 2, 0] = -0.00001
+        pre.encode_image(img)
+
+    with pytest.raises(ValueError):
+        img = np.zeros((10, 10, 3), dtype=int)
+        img[2, 2, 0] = 256
+        pre.encode_image(img)
+
+    with pytest.raises(ValueError):
+        img = np.zeros((10, 10, 3), dtype=int)
+        img[2, 2, 0] = -1
+        pre.encode_image(img)
+
+
+#
+# pre.encode_image(np.zeros((100, 1, 1)))
 
 
 def _debug_preprocessing(expected_preprocessed_image, preprocessed_image):
@@ -59,3 +94,30 @@ def test_matches_original_clip():
     assert np.allclose(
         preprocessed_image, expected_preprocessed_image, atol=1e-6
     )
+
+
+def test_dtypes():
+    image = Image.open(IMAGE_PATH).convert("RGB")
+    pre = Preprocessor()
+
+    image_np_uint8 = np.array(image)
+    assert image_np_uint8.dtype == np.uint8
+
+    image_np_float = image_np_uint8.astype(np.float32) / 255.0
+    image_np_int = image_np_uint8.astype(int)
+
+    prepped_image = pre.encode_image(image)
+    prepped_image_np = pre.encode_image(image_np_uint8)
+    prepped_image_np_float = pre.encode_image(image_np_float)
+    prepped_image_np_int = pre.encode_image(image_np_int)
+
+    assert np.allclose(prepped_image, prepped_image_np)
+    assert np.allclose(prepped_image, prepped_image_np_float)
+    assert np.allclose(prepped_image, prepped_image_np_int)
+
+
+def test_special_shapes():
+    pre = Preprocessor()
+
+    pre.encode_image(np.zeros((100, 1, 3)))  # width 1
+    pre.encode_image(np.zeros((100, 50)))  # grayscale
