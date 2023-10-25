b62f4be4d7a9331d4f1bdea10f7886facfafe6bb
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2023-10-26 06:04:12 +0800

    update(clip2onnx): change demo template

--- examples/demo_clip2onnx.py
+++ examples/demo_clip2onnx.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 
 from clip2onnx.model_card import ModelCard
-from clip2onnx.templates import ViT_B_32, HF_ViT_L_14
+from clip2onnx.templates import ViT_B_32, RN50
 
 project_dir = Path(__file__).parent.parent
 assets_dir = project_dir.joinpath("assets")
-dummy_image_path = assets_dir.joinpath("hello-world.jpg")
+dummy_image_path = assets_dir.joinpath("franz-kafka.jpg")
 model_dir = project_dir.joinpath("model")
 
 
 def demo():
-    for template in [HF_ViT_L_14, ViT_B_32]:
+    for template in [ViT_B_32, RN50]:
         template.update({"model_dir": model_dir})
         model_card = ModelCard.from_template(template)
         model_card(dummy_image_path=dummy_image_path)
